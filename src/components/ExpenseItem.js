4d4736e75827332d7cb8615f232a97a9f4faabae
haqnawaz <haqnawaz99@yahoo.com>
2022-10-08 13:28:05 +0500

    First commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+const ExpenseItem = (props) => {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <h2 className="expense-item__description">{props.title}</h2>
+      <div className="expense-item__price">{props.amount}</div>
+      <button>Change Text</button>
+    </div>
+  );
+};
+export default ExpenseItem;
